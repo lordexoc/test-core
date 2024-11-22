dff942f82a596357e21f6db1aba47946fb152429
Özgür Özdemir <lordexoc@users.noreply.github.com>
2024-11-22 16:02:55 +0300

    Update OdysseyCoreServiceProvider.php

--- src/OdysseyCoreServiceProvider.php
+++ src/OdysseyCoreServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class OdysseyCoreServiceProvider extends ServiceProvider {
 
     public function boot() {
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
         $this->publishes([
             __DIR__ . '/config/odyssey-core.php' => config_path('odyssey-core.php'),
